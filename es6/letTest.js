0643fe530a563fd86e42680377cd5cb74c9c96f9
wudeqin <wudeqin@huafagroup.com>
2019-04-02 23:28:24 +0800

    巩固基础

---
+++ es6/letTest.js
@@ -0,0 +1,32 @@
+// let user = {name : 'wdq', age: 12}
+// const INFO = user;
+// user.name = 'ak'
+// // INFO = null
+// INFO.name = 'w'
+// console.log(INFO)
+//
+// let [a, ...c] = [1, 2, 3]
+// console.log(c)
+
+// let obj = {
+// //     a: 1,
+// //     b: 2,
+// //     c: 3
+// // }
+// //
+// // let {a: A, b, C} = obj
+// //
+// // console.log(A)
+// // console.log(b)
+// var title = 1
+//
+// let template = `
+// <div>
+//     <span>${title}</span>
+// </div>
+// `
+// console.log(template)
+
+let a = Symbol()
+let b = Symbol
+console.log(b)
