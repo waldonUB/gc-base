bfcf222ac9df53ed7d95abd83d23f4ab531ff45f
wudeqin <wudeqin@huafagroup.com>
2019-04-04 15:33:25 +0800

    巩固基础

--- es6/letTest.js
+++ es6/letTest.js
@@ -1,3 +1,4 @@
+'use strict'
 // let user = {name : 'wdq', age: 12}
 // const INFO = user;
 // user.name = 'ak'
@@ -27,6 +28,71 @@
 // `
 // console.log(template)
 
-let a = Symbol()
-let b = Symbol
-console.log(b)
+// let a = Symbol()
+// // let b = Symbol
+// // console.log(b)
+
+// let arr = [1, 2]
+// arr.push(3)
+// // arr.add(4)
+// console.log(arr)
+//
+// let set = new Set([1, 2])
+// set.add(3)
+// const a = '0'
+// if (a) {
+//     console.log(typeof true)
+// }
+// let count = 0
+// const timer = setInterval(() => {
+//     count ++
+//     if (count === 10) {
+//         clearInterval(timer)
+//         return
+//     }
+//     console.log(count)
+// },1000)
+// console.log(`timer` + timer)
+
+// const obj = {
+//     1: 1
+// }
+//
+// console.log(obj.hasOwnProperty())
+
+// let person = {
+//     first_name: 'waldon',
+//     last_name: 'UB',
+//     getFullName: ()=> {
+//         // const _this = this
+//         return this.first_name + this.last_name
+//     }
+// }
+// // let fullName = person.getFullName()
+// console.log(person.getFullName())
+
+// var person = {
+// //     first_name: 'waldon',
+// //     last_name: 'UB',
+// //     getFullName: function () {
+// //         var _this = this
+// //         return _this.first_name + _this.last_name
+// //     }
+// // }
+// // var fullName = person.getFullName
+// // // console.log(person.getFullName())
+// // console.log(fullName())
+let a = 1;
+let promise2 = (function () {
+    return new Promise((resolve, reject) => {
+        if (a) {
+            resolve(a, a)
+        } else {
+            reject(a)
+        }
+    })
+})().then((onFulfilled, onRejected) => {
+    console.log(onFulfilled)
+}).catch(onRejected => {
+    console.log(onRejected)
+})
