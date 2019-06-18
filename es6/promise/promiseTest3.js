8e7764d6fe1037dd121cd73b12898e3fa6429e0c
waldonUB <852727515@qq.com>
2019-06-18 16:15:25 +0800

    更新promise和观察者模式

---
+++ es6/promise/promiseTest3.js
@@ -0,0 +1,47 @@
+function MyPromise(execute) {
+    let state = 'pending'
+    let callbacks = []
+    if (typeof execute !== 'function') {
+        throw new Error('execute必须为函数')
+    }
+    function resolve(value) {
+        state = 'onfulfilled'
+        setTimeout(function () {
+            value = callbacks[0].onfulfilled(value)
+            handler(value)
+        }, 0)
+    }
+    function reject(value) {
+        state = 'onrejected'
+        setTimeout(function () {
+            value = callbacks[0].onrejected(value)
+            handler(value)
+        }, 0)
+    }
+    function handler(value) {
+        for (let i = 1; i < callbacks.length; i++) {
+            value = callbacks[i].onfulfilled(value)
+        }
+    }
+    this.then = function (onfulfilled, onrejected) {
+        callbacks.push({onfulfilled, onrejected})
+        return this
+    }
+    execute(resolve, reject)
+}
+
+function f() {
+    return new MyPromise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(2)
+        }, 0)
+    })
+}
+f().then(result => {
+    console.log(`resolve:` + result)
+    return result + 1
+}, result2 => {
+    console.log(`reject:` + result2)
+}).then(result => {
+    console.log(`第二次then：` + result)
+})
---
+++ js/pattern/observer/observer.js
@@ -0,0 +1,29 @@
+function Observer() {
+    let callbacks = []
+    this.subscribe = function(topic, callback) {
+        callbacks.push({topic, callback})
+    }
+    this.unsubscribe = function(topic) {
+        callbacks = callbacks.filter(item=>
+            item.topic !== topic)
+    }
+    this.notify = function() {
+        setTimeout(function () {
+            callbacks.forEach(item => {
+                item.callback()
+            })
+        }, 0)
+    }
+}
+let observer = new Observer()
+observer.subscribe('eat', function () {
+    console.log(`我回去吃饭了`)
+})
+observer.subscribe('go', function () {
+    console.log(`我下班啦`)
+})
+observer.notify()
+observer.subscribe('go', function () {
+    console.log(`我下班啦2`)
+})
+observer.unsubscribe('eat')
