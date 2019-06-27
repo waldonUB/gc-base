0300aacdd222c4dea4ccd8aa87299f3a62f3c5db
waldonUB <852727515@qq.com>
2019-06-27 16:26:23 +0800

    测试yield，手动实现iterator和for...of

---
+++ es6/iterator/IteratorTest.js
@@ -0,0 +1,18 @@
+function MyIterator(arr) {
+    let index = 0
+    return {
+        next() {
+            if (index < arr.length) {
+                return {
+                    value: arr[index++],
+                    done: false
+                }
+            } else {
+                return {
+                    value: undefined,
+                    done: true
+                }
+            }
+        }
+    }
+}
--- es6/iterator/forOf.js
+++
@@ -1,9 +0,0 @@
-var arr = [1, 2, 3]
-var string = 'waldon'
-for (let item of arr) {
-    console.log(item)
-}
-for (let item of string) {
-    console.log(item)
-}
-console.log(typeof string)
