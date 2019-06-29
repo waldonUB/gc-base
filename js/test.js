76fe2dae23221a43e5dd47ded8cff9a44f6c1681
waldonUB <852727515@qq.com>
2019-06-29 18:13:32 +0800

    测试了一下赌博加倍的期望值，解决了10几年的一个疑惑。。。

---
+++ js/test.js
@@ -0,0 +1,35 @@
+function getNum() {
+    let sum = 0
+    let all = 0
+    while(true) {
+        console.log(`进行了` + (all++) + `次`)
+        let num = Math.random().toString().slice(-2, -1)
+        if (num % 2 === 0) {
+            console.log(`当前是偶数：` + num)
+            sum++
+            console.log(`sum值为：` + sum)
+            let money = all - getMoney()
+            console.log(`本次赚了：` + money)
+        } else {
+            sum = 0
+        }
+        let flag = false
+        if (sum === 9) {
+            flag = true
+        }
+        if (flag) {
+            return
+        }
+    }
+}
+
+function getMoney() {
+    let sum = 0
+    for (let i = 0; i < 10; i++) {
+        sum += 2 ** i
+    }
+    return sum
+}
+
+
+getNum()
