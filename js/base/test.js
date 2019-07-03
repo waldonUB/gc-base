735942b3de37ff0f13cb82158a23256b04a5f28c
waldonUB <852727515@qq.com>
2019-07-03 10:03:13 +0800

    不看源码实现了一个reduce，发现居然和源码差不多，开心0.0

--- js/base/test.js
+++ js/base/test.js
@@ -1,6 +1,16 @@
-console.log(undefined instanceof Object)
-console.log(null instanceof Object)
-console.log(5 instanceof Object)
-console.log("" instanceof Object)
-console.log(true instanceof Object)
-console.log(Symbol instanceof Object)
+// console.log(undefined instanceof Object)
+// console.log(null instanceof Object)
+// console.log(5 instanceof Object)
+// console.log("" instanceof Object)
+// console.log(true instanceof Object)
+// console.log(Symbol instanceof Object)
+function f() {
+    let initList = [...Array(12)].map((item, index) => {
+        return {
+            month: index,
+            value: 0
+        }
+    })
+    console.log(initList)
+}
+f()
