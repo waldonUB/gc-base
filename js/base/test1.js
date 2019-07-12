385ee19da12e9a5947b4adfc7d3ac37ad9fb324c
waldonUB <852727515@qq.com>
2019-07-12 10:20:53 +0800

    测下动态组件缓存销毁的问题

---
+++ js/base/test1.js
@@ -0,0 +1,13 @@
+// function f(param) {
+//     let sum = 1
+//     sum += param || 0
+//     return sum
+// }
+//
+// console.log(f(null));
+// console.log(f(5));
+// console.log(f(''));
+// console.log(f(undefined));
+
+// let x = [].x || 0
+// let y = [].y || 0
