75843bd25bc64f515a0c7a9cbfbb0d17a104af78
waldonUB <852727515@qq.com>
2019-08-04 23:17:29 +0800

    日常练习

---
+++ js/callApplyBind/applyTest1.js
@@ -0,0 +1,17 @@
+function getGlobalObject () {
+  return this
+}
+
+Function.prototype.applyTest = function (thisArg, argArray) {
+  // 三个判断
+  if (typeof this !== 'function') {
+    throw new TypeError("this is not a function")
+  }
+  if (thisArg === null || thisArg === undefined) {
+    thisArg = getGlobalObject()
+  }
+  if (!argArray) {
+    argArray = []
+  }
+
+}
