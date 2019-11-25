feaf301ca799e57ca6510428bbae0560821c6ef6
waldonUB <852727515@qq.com>
2019-11-25 17:27:30 +0800

    测试排序

---
+++ js/base/sortTest.js
@@ -0,0 +1,18 @@
+let arr = [
+    {
+        index: 2
+    },
+    {
+        index: 5
+    },
+    {
+        index: 1
+    },{
+        index: 4
+    }
+]
+
+arr = arr.sort((prev, curr) => {
+    return prev.index - curr.index
+})
+console.log(arr)
