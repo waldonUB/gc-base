2d38ffeb34581382a4af0ac46ae731c0e3d2e5f0
waldonUB <852727515@qq.com>
2019-11-27 12:03:17 +0800

    测试push和concat

---
+++ js/array/spliceTest.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+let arr2 = arr.splice(0, 10)
+console.log(arr)
+console.log(arr2)
--- js/base/dateTest.js
+++ js/base/dateTest.js
@@ -1,6 +1,14 @@
-let startTime = new Date() 
-let endTime = new Date('2019.11.27 00:00:00')
+// let startTime = new Date() 
+// let endTime = new Date('2019.11.27 00:00:00')
+//
+// console.log(endTime.getTime() > startTime.getTime())
+// console.log(startTime.toDateString())
+// console.log(endTime.toDateString())
 
-console.log(endTime.getTime() > startTime.getTime())
-console.log(startTime.toDateString())
-console.log(endTime.toDateString())
+function isExpire() {
+    const currentTime = new Date()
+    const endTime = new Date('2019.11.26 00:00:00')
+    return currentTime > endTime
+}
+
+console.log(isExpire())
