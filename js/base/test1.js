f9000a1e6f583d31d59a1daae69ffdbb59869795
waldonUB <852727515@qq.com>
2019-12-02 20:38:27 +0800

    typeof Test

--- js/base/test1.js
+++ js/base/test1.js
@@ -1,1 +1,9 @@
 console.log(new Date().getTime())
+const a = ''
+const b = undefined
+const c = 0
+const d = '0'
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
