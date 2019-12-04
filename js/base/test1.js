6bfbcd974af690a904d9dfc5885694182e371993
waldonUB <852727515@qq.com>
2019-12-04 17:43:48 +0800

    写一下注释规范

--- js/base/test1.js
+++ js/base/test1.js
@@ -7,3 +7,13 @@ console.log(typeof a)
 console.log(typeof b)
 console.log(typeof c)
 console.log(typeof d)
+
+console.log(Number(''))
+
+/**
+ * 测试保存的方法
+ * @param {number} testData.sort 1:创建时间倒序 2:创建时间顺序 3:阅读人数倒序 4:阅读人数顺序
+ * @then {object} res
+ * */
+function saveTest(testData) {
+}
