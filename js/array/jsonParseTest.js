f646dc21af7f9d42e30c64fd10af69aadcf2d0fa
waldonUB <852727515@qq.com>
2020-03-25 23:41:13 +0800

    测试格式化的不足

---
+++ js/array/jsonParseTest.js
@@ -0,0 +1,12 @@
+let obj = {
+    a: {
+        b: null,
+        c: '',
+        d: undefined,
+        e: () => {},
+        f: []
+    }
+}
+
+let objCopy = JSON.parse(JSON.stringify(obj))
+console.log(objCopy)
