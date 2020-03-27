92e54f4c5c9980f5e74dcb30bc039715d5e23193
waldonUB <852727515@qq.com>
2020-03-27 19:59:01 +0800

    测试跳转，ios未测试

---
+++ es6/base/reBuildTest.js
@@ -0,0 +1,13 @@
+let params = {
+    a: '',
+    b: 'bb',
+    c: 'ww'
+}
+let params2 = {
+    a: 'qqq',
+    b: 'rrrr',
+    c: ''
+}
+Object.assign(params, params2)
+
+console.log(Object.assign(params, params2))
