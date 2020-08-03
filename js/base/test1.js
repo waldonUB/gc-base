9ec42b55f3e071241cc5a0e471f5a37b1bda9923
waldonUB <852727515@qq.com>
2020-08-03 14:42:14 +0800

    测试

--- js/base/test1.js
+++ js/base/test1.js
@@ -1,2 +1,5 @@
-let a = '123'
-console.log(a.slice(0, 2))
+let a = '[123, 456]'
+let aJson = JSON.parse(a)
+console.log(aJson[1])
+let aJsonText = aJson.map(item => item).join('-')
+console.log(`aJsonText`, aJsonText)
