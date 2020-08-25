b8dec26fc0c52583c6c45c3c4b474bcc5430d324
waldonUB <852727515@qq.com>
2020-08-25 10:09:32 +0800

    触摸穿透

--- js/base/test1.js
+++ js/base/test1.js
@@ -1,11 +1,3 @@
-function f() {
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            if (i === 5) {
-                break
-            }
-            console.log(i)
-        }
-    }
-}
-f()
+let a = '1.1'
+
+console.log(typeof a)
