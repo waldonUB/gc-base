e1d11e4561621f91e1f5037aa4f6b297d6ad0d26
waldonUB <852727515@qq.com>
2020-08-27 16:23:58 +0800

    位数置换

--- js/base/test1.js
+++ js/base/test1.js
@@ -1,3 +1,18 @@
-let a = '1.1'
+let testData = '10 20 30 40 50 60'
 
-console.log(typeof a)
+function f(inputParams) {
+    let inputArr = inputParams.split(' ')
+    for (let i = 0; i < inputArr.length; i++) {
+        let a = ''
+        inputArr[i] = Number.parseInt(inputArr[i]).toString(2)
+        for (let j = 0; j < inputArr[i].length; j++) {
+            if (j % 2 === 0) {
+                a += inputArr[i].charAt(j + 1)
+                a += inputArr[i].charAt(j)
+            }
+        }
+        inputArr[i] = a
+    }
+    console.log(inputArr)
+}
+f(testData)
