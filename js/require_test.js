cf0e7d5620031d6f56131e6eabba6cc164f06153
waldonUB <852727515@qq.com>
2021-01-27 21:12:55 +0800

    3

--- js/require_test.js
+++ js/require_test.js
@@ -1,5 +1,7 @@
-console.log(1)
-console.log('waldon1')
+console.log(1);
+console.log("waldon1");
 
-console.log('waldon2')
+console.log("waldon2");
 
+console.log("waldon2");
+console.log("waldon3");
