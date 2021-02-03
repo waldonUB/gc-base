967924d27718eb416d12e67b8fac875ffb16eda9
waldonUB <852727515@qq.com>
2021-02-03 18:50:55 +0800

    增加值班提醒

---
+++ nodeProject/duty-task/mock/testData.js
@@ -0,0 +1,13 @@
+// {
+//   "id": 2,
+//   "name": "前端节假日值班",
+//   "url": "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=81af6f01-46cb-49d9-9893-c51d8c9f4bf5"
+// },
+// {
+//   "id": 3,
+//   "name": "开发易销",
+//   "url": "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0f1afab6-8bb9-4ced-bd06-acd13a9d4bc1"
+// }
+
+
+console.log(new Date().getHours())
