2c9328271f7969d73efe0aaa2296253f4c988a46
waldonUB <852727515@qq.com>
2021-04-07 08:51:19 +0800

    提交

--- nodeProject/webpack-build/webpack.config.js
+++ nodeProject/webpack-build/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs-extra");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const srcDir = path.resolve(__dirname, "src/wxWork");
 const readDir = fs.readdirSync(srcDir);
 let entry = {};
@@ -9,15 +10,15 @@ readDir.forEach((item) => {
   entry[entryName] = path.resolve(srcDir, item);
 });
 
-const testDir = fs.readdirSync('U:\\dev-svn\\waldon\\res\\tsportal\\test')
-console.log(`testDir`, testDir)
+const testDir = fs.readdirSync("U:\\dev-svn\\waldon\\res\\tsportal\\test");
+console.log(`testDir`, testDir);
 
 module.exports = {
-  mode : 'development',
+  mode: "development",
   entry,
   output: {
     filename: "[name].src.js",
-    path: 'U:\\dev-svn\\waldon\\res\\tsportal\\test',
+    path: "U:\\dev-svn\\waldon\\res\\tsportal\\test",
   },
   module: {
     rules: [
@@ -36,5 +37,5 @@ module.exports = {
   watchOptions: {
     ignored: /^((?!wxWork).)*$/,
   },
-  devtool: 'cheap-module-source-map'
+  devtool: "cheap-module-source-map",
 };
