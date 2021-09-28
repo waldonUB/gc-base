bb3eed8fe171ad49aa3bae502d6ddb0eb89f87d1
waldonUB <852727515@qq.com>
2021-09-28 22:55:02 +0800

    vite测试

--- 计算机基础/设计模式/单例模式.js
+++ 计算机基础/设计模式/单例模式.js
@@ -10,6 +10,9 @@ class Singleton {
     if (!_instance) {
       _instance = _getInstance(params)
     }
+    // 构造函数必须返回一个对象，否则不生效
+    // 返回一个指定的对象时，通常用来实现单例
+    // https://www.30secondsofcode.org/articles/s/javascript-return-constructor
     return _instance
   }
 }
@@ -25,3 +28,4 @@ const user2 = new Singleton(params2)
 console.log(`user1`, user1)
 params1.name = 111 // 浅拷贝后不会生效
 console.log(`user2`, user2)
+console.log(user1 === user2)
--- 计算机基础/设计模式/单例模式_function.js
+++ 计算机基础/设计模式/单例模式_function.js
@@ -25,3 +25,4 @@ const user2 = new Singleton(params2)
 console.log(`user1`, user1)
 params1.name = 111 // 浅拷贝后不会生效
 console.log(`user2`, user2)
+console.log(user1 === user2)
