96cf6b2b84fdbf44e677690a0354eee012773a62
waldonUB <852727515@qq.com>
2021-10-21 11:42:24 +0800

    ✨ feat: esm相关测试

---
+++ es6/ESM/script/moduleTest.mjs
@@ -0,0 +1,5 @@
+export const a = 5
+
+export const fn = () => {
+  console.log(`只是fn`)
+}
---
+++ 手写题/实现call.js
@@ -0,0 +1,8 @@
+const myCall = (source, target, ...args) => {}
+
+const obj = {
+  name: '我是名字'
+}
+const fn = () => {
+  console.log(`这个名字：${this.name}`)
+}
