b7bed8254417da4f373d980b2e95e43d2b41426f
waldonUB <852727515@qq.com>
2022-01-20 10:39:53 +0800

    feat: ✨ http2生成证书

--- nodeProject/http-test/router/test/index.js
+++ nodeProject/http-test/router/test/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/cache-test', (req, res) => {
+router.get('/cache-test', async (req, res) => {
   // res.setHeader('Cache-Control', 'max-age=86400')
   console.log(`进来了`)
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 5000)
+  })
+  console.log(`醒了`)
   res.render('test/cache')
 })
 
-router.post('/form-post', (req, res) => {
+router.post('/form-post', async (req, res) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 5000)
+  })
   res.json({
     rt: 0,
     success: true,
