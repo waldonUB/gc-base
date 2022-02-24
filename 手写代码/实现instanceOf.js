fb8fc06b8be30a2059fd66f9bb4aab62a9353b16
waldonUB <852727515@qq.com>
2022-02-24 18:44:34 +0800

    feat: ✨ 轮子实现

---
+++ 手写代码/实现instanceOf.js
@@ -0,0 +1,33 @@
+// todo waldon
+
+/**
+ * 主要是看他在不在构造函数的原型链上
+ * @author waldon
+ * @date 2022-02-24
+ */
+const myInstanceof = function (instance, constructor) {
+  if (typeof constructor !== 'function') {
+    throw Error('not an object')
+  }
+  let flag = false
+  while (instance && !flag) {
+    if (instance['__proto__'] === constructor.prototype) {
+      flag = true
+    }
+    instance = instance['__proto__']
+  }
+  return flag
+}
+
+const Fn = function () {}
+const Child = function () {}
+Child.prototype = new Fn()
+
+const foo = new Fn()
+const child = new Child()
+console.log(foo instanceof Fn)
+console.log(child instanceof Fn)
+console.log(child.__proto__.__proto__ === Fn.prototype)
+
+console.log('myInstanceof: ', myInstanceof(foo, Fn))
+console.log('myInstanceof: ', myInstanceof(child, Fn))
