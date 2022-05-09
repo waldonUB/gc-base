4f3b27a569173df449aac5a08a9c0119b4930115
wadlonUB <852727515@qq.com>
2022-05-09 23:27:31 +0800

    feat: ✨ react项目

--- nodeProject/react-test/src/components/function/todoList/index.jsx
+++ nodeProject/react-test/src/components/function/todoList/index.jsx
@@ -1,6 +1,8 @@
 import { List, Typography, Divider } from 'antd'
+import { Button } from 'antd'
+import { useState } from 'react'
 
-const data = [
+let initData = [
   'Racing car sprays burning fuel into crowd.',
   'Japanese princess to wed commoner.',
   'Australian walks 100km after outback crash.',
@@ -12,7 +14,15 @@ function testClick(rowData) {
   console.log('click: ', rowData)
 }
 
+// todo waldon 写法感觉有点冗余
+function delItem(e, rowData, setData) {
+  e.stopPropagation()
+  initData = initData.filter((item) => item !== rowData)
+  setData(initData)
+}
+
 function TodoList() {
+  const [data, setData] = useState(initData)
   return (
     <div className="todoList">
       <Divider orientation="left">Default Size</Divider>
@@ -24,6 +34,15 @@ function TodoList() {
         renderItem={(item) => (
           <List.Item onClick={testClick.bind(this, item)}>
             <Typography.Text mark>[ITEM]</Typography.Text> {item}
+            <Button
+              type="primary"
+              danger
+              onClick={(e) => {
+                delItem(e, item, setData)
+              }}
+            >
+              删除
+            </Button>
           </List.Item>
         )}
       />
