344a864a46b9aebfed5e79b977f6dc4acaf3f162
waldonUB <852727515@qq.com>
2022-05-09 14:07:22 +0800

    feat: ✨ 继承

--- 手写代码/继承/5-寄生组合继承.js
+++ 手写代码/继承/5-寄生组合继承.js
@@ -1,1 +1,16 @@
-// todo waldon
+function Father(name) {
+  this.name = name
+}
+
+function Son(name, age) {
+  Father.call(this, name) // 1. 调用父级函数（借用构造函数继承） 其实就是把赋值的结果转移到this上了
+  this.age = age
+}
+
+Son.prototype = Object.create(Father.prototype) // 2. 原型继承
+Son.prototype.constructor = Son // 3. 重写构造函数
+
+// 4. 只是一个寄生的扩展，可以不放在主要步骤里面
+Son.prototype.getAge = function () {
+  return this.age
+}
