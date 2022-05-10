343aca55491f882f228328ececcd22edf92120ad
waldonUB <852727515@qq.com>
2022-05-10 14:27:09 +0800

    feat: ✨ 处理redux

--- nodeProject/react-test/src/components/function/dialog/BaseDialog.jsx
+++ nodeProject/react-test/src/components/function/dialog/BaseDialog.jsx
@@ -1,15 +1,51 @@
-import { Button, Modal } from 'antd'
+import { Button, Modal, Input } from 'antd'
 import { useState } from 'react'
+import dialogStore from '@/stores/dialog/index.js'
 
 function BaseDialog() {
+  /* 同步测试逻辑 start */
   const [isShowReduxDialog, setIsShowReduxDialog] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(dialogStore.getState())
+
   const handleOk = () => {
     setIsShowReduxDialog(false)
   }
   const handleCancel = () => {
     setIsShowReduxDialog(false)
   }
+
+  let inputValue = ''
+  const changeInput = (e) => {
+    inputValue = e.target.value
+  }
+
+  const addTodo = () => {
+    console.log('添加')
+    dialogStore.dispatch({
+      type: 'ADD_TODO',
+      text: inputValue,
+    })
+    console.log(dialogStore.getState())
+    setList(dialogStore.getState())
+  }
+  /* 同步测试逻辑 end */
+
+  /* 异步测试逻辑 start */
+  const [asyncText, setAsyncText] = useState(0)
+
+  const changeText = () => {
+    setTimeout(() => {
+      // todo waldon 拆分reducer
+      // todo waldon 还没有看看redux-thunk怎么用
+      dialogStore.dispatch({
+        type: 'CHANGE_TEXT',
+        text: Math.random(),
+      })
+      setAsyncText(dialogStore.getState())
+    }, 1000)
+  }
+
+  /* 异步测试逻辑 end */
 
   return (
     <div className="all-dialog">
@@ -22,12 +58,16 @@ function BaseDialog() {
         >
           测试redux用法
         </Button>
+        <Button type="primary" onClick={changeText}>
+          测试redux-async用法
+        </Button>
       </div>
-      <div className="text-list">
+      <ul className="text-list">
         {list.map((item) => (
-          <Button type=""></Button>
+          <li key={item.text}>{item.text}</li>
         ))}
-      </div>
+      </ul>
+      <ul className="async-wrapper">{asyncText}</ul>
       {/* todo waldon: 暂时不理，好像是antd版本的问题
        Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of DomWrapper which is inside StrictMode */}
       <Modal
@@ -36,7 +76,12 @@ function BaseDialog() {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <p></p>
+        <p>
+          <Input placeholder="输入内容" onChange={changeInput} />
+          <Button type="primary" onClick={addTodo}>
+            添加
+          </Button>
+        </p>
       </Modal>
     </div>
   )
