f59191dd606684a5c770150bd06e90d71f057bd6
waldonUB <852727515@qq.com>
2022-06-01 15:50:07 +0800

    feat: ts部分特效测试

---
+++ typescript-test/src/base/deep_test/any和unknown测试.ts
@@ -0,0 +1,4 @@
+;(function () {
+  const name: unknown = '111'
+  console.log(name)
+})()
--- typescript-test/src/test/test.ts
+++ typescript-test/src/test/test.ts
@@ -1,10 +1,12 @@
-function getConvertStr(str: string) {
-  let res: string = ''
-  for (const item of str) {
-    const _chart: string =
-      item === item.toLocaleLowerCase() ? item.toLocaleUpperCase() : item.toLocaleLowerCase()
-    res += _chart
-  }
-  return res
+// > 用 JavaScript 写一个函数，输入 int 型，返回整数逆序后的字符串。如：输入整型 1234，返回字符串“4321”。要求必须使用递归函数调用，不能用全局变量，输入函数必须只有一个参数传入，必须返回字符串。
+
+// 公司：bilibili
+
+// 解析：[第 99 题](https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/153)
+
+function convert2Str(num: number): string {
+  let str: string = num.toString()
+  return str.length > 1
+    ? convert2Str(Number(str.substring(0, str.length - 1))) + str.substring(str.length - 1)
+    : str
 }
-console.log(`getConvertStr('AbcD')`, getConvertStr('AbcD'))
